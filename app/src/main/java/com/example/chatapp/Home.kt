66950a526306b6fbc8c71a1a32dedeb8dc5ed159
Symonn Ang <symonnjaemur29@gmail.com>
2025-11-25 07:30:21 +0800

    add toolbar

--- app/src/main/java/com/example/chatapp/Home.kt
+++ app/src/main/java/com/example/chatapp/Home.kt
@@ -3,15 +3,26 @@ package com.example.chatapp
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class Home : AppCompatActivity() {
 
@@ -22,6 +33,7 @@ class Home : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var textView: TextView
     private var user: FirebaseUser? = null
+    private lateinit var mDbRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,11 +45,39 @@ class Home : AppCompatActivity() {
             insets
         }
 
+
         userList = ArrayList()
         adapter = UserAdapter(this, userList)
         auth = FirebaseAuth.getInstance();
-        textView = findViewById(R.id.user)
-        logoutButton = findViewById(R.id.logoutBtn)
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+        textView = findViewById(R.id.userName)
+//      logoutButton = findViewById(R.id.logoutBtn)
+
+        userRecyclerView = findViewById(R.id.userRecyclerView)
+        userRecyclerView.layoutManager = LinearLayoutManager(this)
+        userRecyclerView.adapter = adapter
+
+        mDbRef.child("user").addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                userList.clear()
+                for(postSnapshot in snapshot.children) {
+
+                    val currentUser = postSnapshot.getValue(User::class.java)
+                    if(auth.currentUser?.uid != currentUser?.uid) {
+                        userList.add(currentUser!!)
+                    }
+
+                }
+                adapter.notifyDataSetChanged()
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
 //        auth.currentUser?.let {
 //            user = it
 //        } ?: run {
@@ -55,12 +95,41 @@ class Home : AppCompatActivity() {
             textView.text = user?.email
         }
 
-        logoutButton.setOnClickListener {
-            FirebaseAuth.getInstance().signOut();
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+//        logoutButton.setOnClickListener {
+//            FirebaseAuth.getInstance().signOut();
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false) // for title
+        val customTitle = layoutInflater.inflate(R.layout.toolbar_title, null)
+        toolbar.addView(customTitle)
+
+        val profileIcon = findViewById<ImageView>(R.id.profileIcon)
+
+        profileIcon.setOnClickListener {
+            val popup = PopupMenu(this, profileIcon)
+            popup.menuInflater.inflate(R.menu.profile_menu, popup.menu)
+
+            popup.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.action_logout -> {
+                        FirebaseAuth.getInstance().signOut()
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                        true
+                    }
+                    else -> false
+                }
+            }
+
+            popup.show()
         }
 
     }
+
+
 }
